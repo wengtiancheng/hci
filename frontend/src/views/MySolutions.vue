293964c87f3fd6eef65c00a7291dbaa1cc2999a2
王洲权 <221250027@smail.nju.edu.cn>
2024-12-16 23:45:27 +0800

    fix:mySolutions

--- frontend/src/views/MySolutions.vue
+++ frontend/src/views/MySolutions.vue
@@ -41,6 +41,8 @@ const fetchSolutions = async () => {
     console.log('Solutions:', solutions.value);
   } catch (error) {
     console.error('Error fetching solutions:', error);
+  } finally {
+    isLoading.value = false;
   }
 };
 
@@ -66,6 +68,8 @@ const goToCustomBuild = () => {
   router.push({ path: '/custom-build' });
 };
 
+const isLoading = ref(true);
+
 onMounted(() => {
   checkLoginStatus();
 });
@@ -74,41 +78,58 @@ onMounted(() => {
 <template>
   <div class="my-solutions">
     <div v-if="isLoggedIn">
-      <div v-if="solutions.length === 0" class="no-solutions">
-        <img src="../assets/images/no_solution.svg" alt="No Solutions" class="no-solutions-icon" />
-        暂时还没有方案哦
-        <button class="start-journey-button" @click="goToCustomBuild">创建我的方案</button>
+      <div v-if="isLoading" class="loading-overlay">
+        <div class="spinner">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <p class="spinner-text">正在加载...</p>
       </div>
-      <div v-else class="solutions-list">
-        <div
-            v-for="solution in solutions"
-            :key="solution.id"
-            class="solution-card"
-        >
-          <div class="solution-header">
-            <h2 class="solution-name">{{ solution.name }}</h2>
-            <img
-                src="../assets/images/delete.svg"
-                alt="Delete"
-                class="delete-icon"
-                @click="showDeleteDialog(solution.id)"
-            />
-          </div>
-
-          <div class="solution-items">
-            <div v-for="item in solution.items" :key="item.image" class="solution-item">
+      <div v-else>
+        <div v-if="solutions.length === 0" class="no-solutions">
+          <img src="../assets/images/no_solution.svg" alt="No Solutions" class="no-solutions-icon" />
+          暂时还没有方案哦
+          <button class="start-journey-button" @click="goToCustomBuild">创建我的方案</button>
+        </div>
+        <div v-else class="solutions-list">
+          <div
+              v-for="solution in solutions"
+              :key="solution.id"
+              class="solution-card"
+          >
+            <div class="solution-header">
+              <h2 class="solution-name">{{ solution.name }}</h2>
               <img
-                  :src="item.image"
-                  :alt="item.name"
-                  class="solution-item-image"
+                  src="../assets/images/delete.svg"
+                  alt="Delete"
+                  class="delete-icon"
+                  @click="showDeleteDialog(solution.id)"
               />
-              <span class="solution-item-name">{{ item.name }}</span>
             </div>
-          </div>
 
-          <div class="solution-footer">
-            <span class="solution-price">总价: ￥{{ solution.totalPrice }}</span>
-            <button class="detail-button" @click="goToSolutionDetail(solution)">详情</button>
+            <div class="solution-items">
+              <div v-for="item in solution.items" :key="item.image" class="solution-item">
+                <img
+                    :src="item.image"
+                    :alt="item.name"
+                    class="solution-item-image"
+                />
+                <span class="solution-item-name">{{ item.name }}</span>
+              </div>
+            </div>
+
+            <div class="solution-footer">
+              <span class="solution-price">总价: ￥{{ solution.totalPrice }}</span>
+              <button class="detail-button" @click="goToSolutionDetail(solution)">详情</button>
+            </div>
           </div>
         </div>
       </div>
@@ -132,6 +153,109 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+
+.spinner {
+  position: absolute;
+  width: 9px;
+  height: 9px;
+}
+
+.spinner-text {
+  margin-top: 100px;  /* 控制文字与旋转器之间的距离 */
+  font-size: 16px;
+  color: #333;
+}
+
+.spinner div {
+  position: absolute;
+  width: 50%;
+  height: 150%;
+  background: #001657;
+  transform: rotate(calc(var(--rotation) * 1deg)) translate(0, calc(var(--translation) * 1%));
+  animation: spinner-fzua35 1s calc(var(--delay) * 1s) infinite ease;
+}
+
+.spinner div:nth-child(1) {
+  --delay: 0.1;
+  --rotation: 36;
+  --translation: 150;
+}
+
+.spinner div:nth-child(2) {
+  --delay: 0.2;
+  --rotation: 72;
+  --translation: 150;
+}
+
+.spinner div:nth-child(3) {
+  --delay: 0.3;
+  --rotation: 108;
+  --translation: 150;
+}
+
+.spinner div:nth-child(4) {
+  --delay: 0.4;
+  --rotation: 144;
+  --translation: 150;
+}
+
+.spinner div:nth-child(5) {
+  --delay: 0.5;
+  --rotation: 180;
+  --translation: 150;
+}
+
+.spinner div:nth-child(6) {
+  --delay: 0.6;
+  --rotation: 216;
+  --translation: 150;
+}
+
+.spinner div:nth-child(7) {
+  --delay: 0.7;
+  --rotation: 252;
+  --translation: 150;
+}
+
+.spinner div:nth-child(8) {
+  --delay: 0.8;
+  --rotation: 288;
+  --translation: 150;
+}
+
+.spinner div:nth-child(9) {
+  --delay: 0.9;
+  --rotation: 324;
+  --translation: 150;
+}
+
+.spinner div:nth-child(10) {
+  --delay: 1;
+  --rotation: 360;
+  --translation: 150;
+}
+
+@keyframes spinner-fzua35 {
+  0%, 10%, 20%, 30%, 50%, 60%, 70%, 80%, 90%, 100% {
+    transform: rotate(calc(var(--rotation) * 1deg)) translate(0, calc(var(--translation) * 1%));
+  }
+
+  50% {
+    transform: rotate(calc(var(--rotation) * 1deg)) translate(0, calc(var(--translation) * 1.5%));
+  }
+}
 
 .no-solutions {
   display: flex;
